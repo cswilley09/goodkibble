49d569f7817b585b5823cdf656899991fcae60d9
cswilley09 <willeysconnor@gmail.com>
2026-03-05 22:16:30 -0700

    Delete app/next.config.js

--- app/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 's7d2.scene7.com' },
-    ],
-  },
-};
-module.exports = nextConfig;
